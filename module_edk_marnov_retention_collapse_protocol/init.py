c518a3c001829a0a61024c029482068c871cfe35
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-06-25 02:11:56 +0200

    Initialize init.py with protocol imports and exports

---
+++ module_edk_marnov_retention_collapse_protocol/init.py
@@ -0,0 +1,11 @@
+from .marnov_retention_collapse_protocol import (
+    EDKMarnovProtocolLogger,
+    EDKMarnovRetentionCollapseProtocol,
+    MarnovRetentionCollapseConfig,
+)
+
+__all__ = [
+    "EDKMarnovProtocolLogger",
+    "EDKMarnovRetentionCollapseProtocol",
+    "MarnovRetentionCollapseConfig",
+]
